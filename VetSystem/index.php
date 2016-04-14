743bfcf08a3ab6bf292f9d4776d82a1c7360b18c
paintedlife <franticreader_11@yahoo.com>
2016-04-14 19:23:13 +0900

    Update_1

--- VetSystem/index.php
+++ VetSystem/index.php
@@ -18,7 +18,20 @@
 					<label for = "password" class = "label-italic">Password: </label>
 					<input type = "password" name = "pword" id = "password" class = "input-small" required><p>
 					<a href = "#" class = "fp"> Forgot Password? </a>
+					<p>
 					<input type = "submit" name = "login" value = "Login" class = "input-button">
+				</form>
+				</div>
+				<div class = "register">
+				<div class = "header-bold"> Register: </div>
+				<form>
+					<label for = "username2" class = "label-italic">Username: </label>
+					<input type = "text" name = "uname" id = "username2" class = "input-small" required><p>
+					<label for = "password2" class = "label-italic">Password: </label>
+					<input type = "password" name = "pword" id = "password2" class = "input-small" required><p>
+					<label for = "email" class = "label-italic">Email: </label>
+					<input type = "email" name = "email" id = "email" class = "input-small" required><p>
+					<input type = "submit" name = "register" value = "Register" class = "input-button">
 				</form>
 				</div>
 			</div>
