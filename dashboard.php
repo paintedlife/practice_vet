c1eab391996e1c1e44288dbd1d7f69dfd3014ebf
paintedlife <franticreader_11@yahoo.com>
2016-04-25 22:14:05 +0900

    Added files via upload

---
+++ dashboard.php
@@ -0,0 +1,96 @@
+<html>
+	<head>
+	<link rel="stylesheet" href="css/general.css">
+	<link rel="stylesheet" href="css/input.css">
+	<link rel="stylesheet" href="css/label.css">
+	<link rel="stylesheet" href="css/header.css">
+	<link rel="stylesheet" href="css/list.css">
+	<link rel="stylesheet" href="css/icons.css">
+	<link rel="stylesheet" href="css/span.css">
+	<title> Dashboard </title>
+	</head>
+	<body>
+	
+	<div id = "container">
+	
+		<div id = "header">
+			<div class = "header-logo">
+			<a href = "#" class = "sign-link"><img src = "img/paw.png" class = "logo"> Dashboard</a>
+			</div>
+		</div>
+	
+		<div id = "sidebar">
+			<div class = "img-avatar">
+				<img src = "img/Derp.jpg" class = "avatar">
+				<h4 class = "avatar-name"> Derp The Majestic Man</h4>
+				<a href = "#" class = "signout"> sign-out </a>
+			</div>
+			<div class = "sidenav">
+				<ul>
+					<li class = "option"><a href = "#" class = "opt"><i class = "icon-profile"></i>Profile</a></li>
+					<li class = "option"><a href = "#" class = "opt"><i class = "icon-paw"></i>Pets</a></li>
+					<li class = "option"><a href = "#" class = "opt"><i class = "icon-notif"></i>Notifications</a></li>
+					<li class = "option"><a href = "#" class = "opt"><i class = "icon-pet"></i>Other shit</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<div id = "content">
+		
+			<div class = "content-title">
+				<h2 class = "cn-header"> Your Pets </h2>
+			</div>
+			
+			<div class = "content-inside">
+				<h4 class = "cn-in-header">Name: Doge </h4>
+			</div>
+			<div class = "content-subject">
+				<div class = "cn-sb-avatar">
+					<a href = "#">
+					<div class = "text-content"> View Pet Profile </div>
+					<img src = "img/doge.jpg" class = "cn-avatar">
+					</a>
+				</div>
+					<div class = "content-subject-info">
+						<h3 class = "cn-sb-header"> Information </h3>
+						<ul>
+							<li class = "cn-sb-info"> Condition: Sick </li>
+							<li class = "cn-sb-info"> Visit: 5 </li>
+							<li class = "cn-sb-info"> Shots taken: 3 </li>
+							<li class = "cn-sb-info"> Expected life span: </li>
+						</ul>
+							<div class = "meter">
+								<span style = "width: 100%;"></span>
+							</div>
+					</div>
+			</div>
+			
+			<div class = "content-inside">
+				<h4 class = "cn-in-header">Name: Nyaa </h4>
+			</div>
+			<div class = "content-subject">
+				<div class = "cn-sb-avatar">
+					<a href = "#">
+					<div class = "text-content"> View Pet Profile </div>
+					<img src = "img/nyaa.jpg" class = "cn-avatar">
+					</a>
+				</div>
+					<div class = "content-subject-info">
+						<h3 class = "cn-sb-header"> Information </h3>
+						<ul>
+							<li class = "cn-sb-info"> Condition: Good </li>
+							<li class = "cn-sb-info"> Visit: 3 </li>
+							<li class = "cn-sb-info"> Shots taken: 1 </li>
+							<li class = "cn-sb-info"> Expected life span: </li>
+						</ul>
+							<div class = "meter">
+								<span style = "width: 50%;"></span>
+							</div>
+					</div>
+			</div>
+		
+		</div>
+		
+	</div>
+	</body>
+</html>
